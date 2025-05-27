78bf09f9d946862a443effca1601d1a5885a9463
Akshae815 <matta.akshaee@gmail.com>
2025-05-27 14:16:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,53 +3,40 @@ import pandas as pd
 import streamlit as st
 import pickle
 
-with open('final_model.pkl','rb')as file:
-    model=pickle.load(file)
+# Load model and transformer
+with open('final_model.pkl', 'rb') as file:
+    model = pickle.load(file)
 
-with open('transformer.pkl','rb')as file:
-    transformer=pickle.load(file)
+with open('transformer.pkl', 'rb') as file:
+    transformer = pickle.load(file)
 
-def prediction (input_list):
-    input_list=np.array(input_list,dtype=object)
-
-    pred=model.predict_proba([input_list])[:,1][0]
-
-    if pred>0.5:
-        return f'This booking is more likely cancelled with chances{round(pred,2)}.'
-
+# Prediction function
+def prediction(input_list):
+    input_list = np.array(input_list, dtype=object)
+    pred = model.predict_proba([input_list])[:, 1][0]
+    if pred > 0.5:
+        return f'This booking is more likely cancelled with chances {round(pred, 2)}.'
     else:
-        return f'This booking is less likely to get cancelled with chances{round(pred,2)}.'
+        return f'This booking is less likely to get cancelled with chances {round(pred, 2)}.'
 
+# Streamlit UI
 def main():
-    st.title('IN HOTEL GROUP')
-    lt = st.text_input('Enter The Lead Time in Days:')  
-    mkt=(lambda x:1 if x=='Online' else 0)(st.selectbox('How the booking is made',['Online','Offline']))
-    price =st.text_input('Enter the price of the room ')
-    adult = st.selectbox('How many adult',[1,2,3,4])
-    arr_m = st.slider('What is th month of arrival',min_value=1,max_value=12,step=1)
-    weekd_lambda = (lambda x:0 if x=='Mon' else
-                             1 if x=='Tues'else
-                             2 if x=='Wed' else
-                             3 if x=="Thu"else
-                             4 if x=='Fri' else
-                             5 if x=='Sat' else 6)
-    arr_w = weekd_lambda(st.selectbox('What is weekday of arrival ?',['Mon','Tues','Wed','Thu','Fri','Sat','Sun']))
-    dee_w = weekd_lambda(st.selectbox('What is weekday of departure  ?',['Mon','Tues','Wed','Thu','Fri','Sat','Sun']))
-    weekn =st.text_input('Enter the number of week night in stay')
-    wkndn = st.text_input('enter the number of weekend nights in stay')
-    totan =weekn+wkndn
-    park = (lambda x:1 if x =='Yes'else 0)(st.selectbox('Does customer need parking ?',['Yes','No']))
-    spcl = st.selectbox('How many special requests have been made ?',[0,1,2,3,4,5])
+    st.title('IN HOTEL GROUP - Booking Cancellation Predictor')
 
-    lt_t,price_t=transformer.transform([[lt,price]])[0]
+    # Inputs
+    lt = float(st.text_input('Enter the Lead Time in Days:', value='0'))  
+    mkt = st.selectbox('How the booking is made', ['Online', 'Offline'])
+    mkt = 1 if mkt == 'Online' else 0
 
-    inp_list= [lt_t,spcl,price_t,adult,wkndn,park,weekn,mkt,arr_m,arr_w,totan,dep_w]
-
-    if st.button('Predict'):
-        response = prediction(inp_list)
-        st.success(response)
-
-if __name__=='__main__':
-    main()
+    price = float(st.text_input('Enter the price of the room:', value='0'))
+    adult = st.selectbox('How many adults?', [1, 2, 3, 4])
     
+    arr_m = st.slider('Month of arrival:', min_value=1, max_value=12, step=1)
+
+    weekd_lambda = lambda x: {'Mon': 0, 'Tues': 1, 'Wed': 2, 'Thu': 3, 'Fri': 4, 'Sat': 5, 'Sun': 6}[x]
+    arr_w = weekd_lambda(st.selectbox('Weekday of arrival?', ['Mon', 'Tues', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']))
+    dep_w = weekd_lambda(st.selectbox('Weekday of departure?', ['Mon', 'Tues', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']))
     
+    weekn = int(st.text_input('Enter number of weeknights in stay:', value='0'))
+    wkndn = int(st.text_input('Enter number of weekend nights in stay:', value='0'))
+    totan = weekn + wkndn
